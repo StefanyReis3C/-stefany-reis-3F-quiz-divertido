ab0d6b9b72ca61d654bdbe89092f30d667aef4dc
StefanyReis3C <0000111105891XSP@al.educacao.sp.gov.br>
2024-11-14 16:13:47 -0300

    Add files via upload

---
+++ quiz divertido/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é P na programação?',
+    'O p é para se escrever algo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Rio de Janeiro?',
+    'A capital do Rio é Rio de janeiro'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um title?',
+    'Permite você criar o titulo da página'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz legal em ingês?',
+    'se diz cool (cuul)'
+)
